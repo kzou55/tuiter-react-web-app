ce878194fb6e396de233097579186c6109c33c01
Ken Zou <kzou55@gmail.com>
2022-10-23 21:27:57 -0400

    fixed position on icon

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -7,7 +7,9 @@ const ExploreComponent = () => {
             <div className="row">
                 <div className="col-11 position-relative">
                     <input placeholder="Search Tuiter" className="form-control rounded-pill ps-5"/>
-                    <i className="bi bi-search position-absolute wd-nudge-up"></i>
+                    <span className="position-absolute top-0 mt-2 ms-3"><i
+                        className="bi bi-search"></i>
+                    </span>
                 </div>
                 <div className="col-1">
                     <i className="wd-bottom-4 text-primary float-end bi bi-gear-fill fs-2 position-relative"></i>
